e51b116ff01587607ec8d33cb9275265a57791d1
maulana saputra <maulana.saputra@aqi.co.id>
2023-01-25 11:03:30 +0700

    fix warning
    Signed-off-by: maulana saputra <maulana.saputra@aqi.co.id>

--- openai-manager.php
+++ openai-manager.php
@@ -8,13 +8,13 @@ class cURL
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => $this->timeout,
+            CURLOPT_TIMEOUT => 60,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => $method,
-            CURLOPT_POSTFIELDS => array_key_exists('file', $data) ? $data : json_encode($data),
+            CURLOPT_POSTFIELDS => !empty($data) && array_key_exists('file', $data) ? $data : json_encode($data),
             CURLOPT_HTTPHEADER => [
-                array_key_exists('file', $data) ? "Content-Type: multipart/form-data" : "Content-Type: application/json",
+                !empty($data) && array_key_exists('file', $data) ? "Content-Type: multipart/form-data" : "Content-Type: application/json",
                 "Authorization: Bearer {$this->key}"
             ]
         ];
